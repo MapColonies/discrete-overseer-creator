e9ad73ab21213dff34710e4dd7530082a82b71b1
nitzanmorr <nitzan.morr@gmail.com>
2023-10-30 17:33:52 +0200

    chore: modified values arrays added logs and modified pvc names

--- src/utils/zoomToResolution.ts
+++ src/utils/zoomToResolution.ts
@@ -3,13 +3,17 @@ import { degreesPerPixelToZoomLevel } from '@map-colonies/mc-utils';
 import { ITaskZoomRange } from '../jobs/interfaces';
 import { SERVICES } from '../common/constants';
 import { IConfig } from '../common/interfaces';
+import { Logger } from '@map-colonies/js-logger';
+import { isArray } from 'lodash';
 
 @singleton()
 export class ZoomLevelCalculator {
   private readonly zoomRanges: ITaskZoomRange[];
 
-  public constructor(@inject(SERVICES.CONFIG) private readonly config: IConfig) {
-    const batches = config.get<string[]>('tiling.zoomGroups');
+  public constructor(@inject(SERVICES.CONFIG) private readonly config: IConfig, @inject(SERVICES.LOGGER) private readonly logger: Logger) {
+    const batches = config.get<string>('tiling.zoomGroups');
+    logger.info({ msg: `batches: ${batches}, ${isArray(batches)} type: ${typeof batches}` });
+
     this.zoomRanges = this.getZoomRanges(batches);
   }
 
