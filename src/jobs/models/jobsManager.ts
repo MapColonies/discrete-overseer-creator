1bbbc9f2a1938aff9338aeae28ac28533654db39
razbroc <77406876+razbroc@users.noreply.github.com>
2023-11-06 14:37:07 +0200

    fix: update metadata on successfull end of update process (MAPCO-3528) (#28)
    * chore: update metadata on completed job
    
    * fix: fix tests accordingly
    
    * fix: proccess order

--- src/jobs/models/jobsManager.ts
+++ src/jobs/models/jobsManager.ts
@@ -234,17 +234,17 @@ export class JobsManager {
         msg: `Merging catalog record ${catalogRecord.metadata.id as string} with new metadata`,
       });
 
-      const mergedData = this.metadataMerger.merge(catalogRecord.metadata, job.metadata);
-      this.logger.debug({
-        jobId: job.id,
-        taskId: task.id,
-        internalId: catalogRecord.metadata.id,
-        mergedData: inspect(mergedData),
-        msg: `Updating catalog record ${catalogRecord.metadata.id as string} with merged metadata`,
-      });
-      await this.catalogClient.update(catalogRecord.metadata.id as string, mergedData);
-
       if (job.isSuccessful) {
+        const mergedData = this.metadataMerger.merge(catalogRecord.metadata, job.metadata);
+        this.logger.debug({
+          jobId: job.id,
+          taskId: task.id,
+          internalId: catalogRecord.metadata.id,
+          mergedData: inspect(mergedData),
+          msg: `Updating catalog record ${catalogRecord.metadata.id as string} with merged metadata`,
+        });
+        await this.catalogClient.update(catalogRecord.metadata.id as string, mergedData);
+
         const message = `Updating status of job ${job.id} to be ${OperationStatus.COMPLETED}`;
         this.logger.info({
           jobId: job.id,
