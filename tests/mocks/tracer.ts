a75f83073962b8b856507ff745b31a1b5afe6103
Ran Elishayev <rannygmx@gmail.com>
2024-01-02 14:33:20 +0200

    fix lint and tests

--- tests/mocks/tracer.ts
+++ tests/mocks/tracer.ts
@@ -2,10 +2,16 @@ import { Tracer } from '@opentelemetry/api';
 
 const startSpanMock = jest.fn();
 const startActiveSpanMock = jest.fn();
+const setStatusMock = jest.fn();
+const recordExceptionMock = jest.fn();
+const endMock = jest.fn();
 
 const tracerMock = {
   startSpan: startSpanMock,
   startActiveSpan: startActiveSpanMock,
+  setStatus: setStatusMock,
+  recordException: recordExceptionMock,
+  end: endMock,
 } as unknown as Tracer;
 
-export { tracerMock, startSpanMock, startActiveSpanMock };
+export { tracerMock, startSpanMock, startActiveSpanMock, setStatusMock, recordExceptionMock, endMock };
