3dce78c6002cc56546325ed5ece83e4e79bdde95
SHLOMIKO <shlomiko@rafael.co.il>
2024-03-12 18:45:16 +0200

    chore: fixed prettier issues

--- src/utils/pixelSizeValidate.ts
+++ src/utils/pixelSizeValidate.ts
@@ -1,8 +1,8 @@
 import config from 'config';
 
 export const pixelSizeValidate = (requestedPixelSize: number, sourcePixelSize: number): boolean => {
-    const resolutionTolerance = config.get<number>('resolutionTolerance');
-    const fixedRequestedPixelSize = parseFloat(requestedPixelSize.toFixed(resolutionTolerance));
-    const fixedSourcePixelSize = parseFloat(sourcePixelSize.toFixed(resolutionTolerance));
-    return fixedRequestedPixelSize >= fixedSourcePixelSize;
-}
+  const resolutionTolerance = config.get<number>('resolutionTolerance');
+  const fixedRequestedPixelSize = parseFloat(requestedPixelSize.toFixed(resolutionTolerance));
+  const fixedSourcePixelSize = parseFloat(sourcePixelSize.toFixed(resolutionTolerance));
+  return fixedRequestedPixelSize >= fixedSourcePixelSize;
+};
